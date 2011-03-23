1777bfc023e914326b2e96e17319c249ef4796aa
aruseni <aruseni.magiku@gmail.com>
2011-03-23 01:42:30 +0100

    Add a method for adding new command strings

--- tc/urls.py
+++ tc/urls.py
@@ -4,6 +4,7 @@ from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('tc.views',
     (r'^$', 'index'),
+    (r'^post/$', 'add_command_string'),
     (r'^cs/(?P<command_string_id>\w+)/$', 'command_string_detail'),
     (r'^cs/(?P<command_string_id>\w+)/text/$', 'command_string_text'),
     (r'^cs/$', redirect_to, {'url': '/'}),
--- tc/views.py
+++ tc/views.py
@@ -2,7 +2,8 @@
 
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from tc.models import CommandString
 
@@ -18,3 +19,14 @@ def command_string_detail(request, command_string_id):
 def command_string_text(request, command_string_id):
     command_string = get_object_or_404(CommandString, string_id=command_string_id)
     return HttpResponse(command_string.command_string, mimetype="text/plain")
+
+def add_command_string(request):
+    command_string_string = request.POST.get('command', '')
+    if not command_string_string:
+        return HttpResponseRedirect(reverse('tc.views.index'))
+    command_string = CommandString()
+    command_string.command_string = command_string_string
+    command_string.save()
+    # Reload the object to get the string_id added by the save() method
+    command_string = CommandString.objects.get(id=command_string.id)
+    return HttpResponseRedirect(reverse('tc.views.command_string_detail', args=(command_string.string_id,)))
