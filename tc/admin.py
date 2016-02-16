98595002535354006bdba5345bfaab51e7ef4e0b
aruseni <aruseni.magiku@gmail.com>
2016-02-16 11:05:12 +0200

    Add ModelAdmin for CommandString

--- tc/admin.py
+++ tc/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from tc.models import CommandString
 
-admin.site.register(CommandString)
+class CommandStringAdmin(admin.ModelAdmin):
+    list_display = ("string_id", "datetime_added")
+    list_filter = ("datetime_added",)
+    search_fields = ["string_id", "command_string"]
+
+admin.site.register(CommandString, CommandStringAdmin)
